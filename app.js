524aef2d3bfabe8b22bb3b7c1b72c5dfe30c9321
Michael Skorin <mike.skorin@gmail.com>
2020-07-07 13:25:30 +0300

    Add more environment vars

--- app.js
+++ app.js
@@ -24,8 +24,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = mongoose.model('User', userSchema);
 
